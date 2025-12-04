939c633ced14a8093bcfd6dfa4a678c5d98d5675
Chandraprakash Patra <Chandraprakashpatra2002@gmail.com>
2025-12-05 00:05:09 +0530

    production patching

--- server.py
+++ server.py
@@ -43,6 +43,11 @@ def upload():
     #     fp.write(str(resp))
 
     return "OK"
+# local test 
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8000)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
+
